b0f532964ec7472942cfe1611c563737970e32ad
ashish kumar <ashish@ashishs-MacBook-Pro.local>
2026-02-18 13:43:00 +0530

    Removed duplicate code

--- src/main/java/com/ashish/java/learn/java8/section08/multipleinterface/Interface1.java
+++
@@ -1,7 +0,0 @@
-package com.ashish.java.learn.java8.section08.multipleinterface;
-
-public interface Interface1 {
-    default void methodA() {
-        System.out.println("Interface1.methodA()");
-    }
-}
--- src/main/java/com/ashish/java/learn/java8/section08/multipleinterface/Interface3.java
+++
@@ -1,7 +0,0 @@
-package com.ashish.java.learn.java8.section08.multipleinterface;
-
-public interface Interface3 extends Interface2  {
-    default void methodC() {
-        System.out.println("Interface3.methodC()");
-    }
-}
